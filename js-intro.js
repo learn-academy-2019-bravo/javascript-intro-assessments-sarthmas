308215663d9de28b2b85b60adfc1c5f66c98cb65
sarthmas <sarah@Julias-Air.localdomain>
2019-04-23 13:54:09 -0700

    completed first assessment

--- js-intro.js
+++ js-intro.js
@@ -1,4 +1,4 @@
-// Intro to Javascript Assessments hi
+// Intro to Javascript Assessments
 
 // Consider this variable:
 
@@ -6,17 +6,32 @@ var mantra = "Be the dev";
 
 // 1. Write the code that returns 'B' from mantra.
 
+var qOne = mantra.charAt(0);
+
+console.log(qOne);
+
 // 2. Write the code that determines if there is a 'x' in mantra.
+
+var qTwo = mantra.includes("x");
+
+console.log(qTwo);
 
 // 3. Write the code that determines if there is a 'v' in mantra.
 
+var qThree = mantra.includes("v");
+
+console.log(qThree);
+
 // Stretch: Write the code that returns the position of 'v' in mantra.
 
+var qFour = mantra.indexOf("v");
+
+console.log(qFour);
 
 // Consider the following statement:
 
-var codingIsEasy
-var imBored
+var codingIsEasy = true
+var imBored = true
 
 if (codingIsEasy && imBored) {
     console.log("I need more!")
@@ -24,12 +39,13 @@ if (codingIsEasy && imBored) {
     console.log("Steady as she goes")
 }
 
-// 4. Update the codingIsEasy and imBored variables above the statement to the values required to log "I need more!"
+// 4. Update the codingIsEasy and imBored
+//variables above the statement to the values required to log "I need more!"
 
 // Consider the following statement:
 
-var imLost
-var imFrustrated
+var imLost = false
+var imFrustrated = false
 
 if (imLost || imFrustrated) {
     console.log('Break time!')
@@ -37,26 +53,61 @@ if (imLost || imFrustrated) {
     console.log('Keep coding!')
 }
 
-// 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
+// 5. Update the imLost and imFrustated variables above the statement
+//to the values required to log "Keep coding!"
 
-// 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
+// 6a. Recall the mantra variable from the first question.
+//Write the code that logs each letter of mantra using a for loop.
+
+var mantra = "Be the dev";
+
+var newMantra = mantra.split ("");
+
+for (let i = 0; i < newMantra.length; i++) {
+
+  console.log (newMantra[i]);
+}
 
 // Stretch: Using a while loop.
-
 // Super Stretch: Using forEach().
-
 // Consider the following variable:
+
+
+
+// 7. Write a function that takes a string like message
+//as an argument and returns the string without vowels.
 
 var message = "thisisateststring"
+var message = "thisisateststring"
+const vowels = ["a","e","i","o","u"]
 
-// 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+removeVowels = (string) => {
+    var arr1 = []
+    var split1 = string.split("")
+    for (let i=0; i<split1.length; i++){
+        if (!vowels.includes(split1[i])){
+          arr1.push(split1[i])
+        }
+      }
+      return finalArr = arr1.join("")
+    }
+    console.log(removeVowels(message));
 
-// Stretch: Update your function to throw an exception if the argument is not a string.
+/* Stretch: Update your function to throw an exception if
+the argument is not a string. */
 
-// Super Duper Stretch: Update your function to throw a TypeError if the argument is not a string.
+/* Super Duper Stretch: Update your function to throw a
+TypeError if the argument is not a string. */
 
 // Consider the following variable:
+
+// var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
+
+// 8. Write a function that takes an array like toonimals and
+//returns an array with only the toons that are cats.
 
 var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
 
-// 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+var animal = toonimals.filter(value => value.animal === "cat")
+
+console.log(animal);
--- js-intro.md
+++ js-intro.md
@@ -3,13 +3,25 @@
 Answer these questions, then follow the instructions to submit your first assessment!
 
 #### 1. What is a function? Why would you use one?
+A function is a set of instructions that generates useful new information.  The function itself is a way to expand the capability of computers to generate information by taking variables and transforming them into a new piece of information named "Function".  You would use a function for any purpose, including for example: to perform a mathematical equation, sort a list, pull data out of a list, perform a loop, generate results of a comparison, etc.   Functions are the building blocks of programs in JavaScript and allow a program to broken into smaller steps.  One type of function is a method.
 
 #### 2. What is a method?
+In JavaScript, a method is always a function, but a function is not always a method.  A method is a function which is a property of an object.   An object is a collection of properties, and a property is an association between a name (or key) and a value.  A property's value can be a function, in which case the property is known as a method.
 
 #### 3. What are some examples of javascript string methods?
+.length
+.slice()
+.charAt()
+.match()
+.replace()
+.indexOf()
+.repeat()
 
 #### 4. Explain the difference between accessor and mutator methods. Why would you use one over the other?
+An accessor allows you to get data while a mutator allows you to change data.  If you wanted to change data,  you would use a mutator.  If you want to pull out certain data you would use an accessor.
 
 #### 5. Explain the differences between console.log() and return. Why would you use one over the other?
+Return produces a value in the computer program but the user does not see the result that is retuned on the screen.  The value can be used in subsequent steps of the program - but that process is internal and not visible to the user.
 
-#### 6. What is one thing we did in class this week that helped you learn the most?  
+#### 6. What is one thing we did in class this week that helped you learn the most?
+I have only been in class on Thursday and Friday so I have limited information on all of the activities of the week.  However, I found the Friday morning lecture on objects to be very helpful.
